2d721f55e62c3d54552166c615357cc5fe9fca51
Sean Gilligan <sean@msgilligan.com>
2020-09-02 10:58:50 -0700

    Re-order lines in module-info.java

--- supernautfx/src/main/java/module-info.java
+++ supernautfx/src/main/java/module-info.java
@@ -17,11 +17,6 @@
  * Supernaut.FX module.
  */
 module org.consensusj.supernautfx {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires io.micronaut.inject;
-    requires javax.inject;
-
     requires org.slf4j;
 
     /* TODO: Move java.logging, test support to another module */
@@ -33,13 +28,19 @@ module org.consensusj.supernautfx {
     exports org.consensusj.supernaut;
     exports org.consensusj.supernaut.services;
 
+    requires javafx.graphics;
+    requires javafx.fxml;
+
     exports org.consensusj.supernautfx;
     exports org.consensusj.supernautfx.internal to javafx.graphics;
+    exports org.consensusj.supernautfx.test;
 
     /* TODO: Micronaut subpackage should maybe be internal or in a separate module? */
+    requires io.micronaut.inject;
+    requires javax.inject;
+
     exports org.consensusj.supernautfx.micronaut;
     /* TODO: Fix this */
     /* We have to open this to so Micronaut (possibly in the merged module) can @Inject private fields in it */
     opens org.consensusj.supernautfx.micronaut;
-    exports org.consensusj.supernautfx.test;
 }
