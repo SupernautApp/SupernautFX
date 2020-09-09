3de47df29f946f28ff25a0801d81ca54593548ed
Sean Gilligan <sean@msgilligan.com>
2020-09-08 22:11:40 -0700

    HelloForegroundApp includes Micronaut @Factory

--- supernautfx-sample-hello/src/main/java/app/supernaut/fx/sample/hello/HelloForegroundApp.java
+++ supernautfx-sample-hello/src/main/java/app/supernaut/fx/sample/hello/HelloForegroundApp.java
@@ -19,6 +19,7 @@ import app.supernaut.fx.FxLauncher;
 import app.supernaut.fx.FxForegroundApp;
 import app.supernaut.fx.FxmlLoaderFactory;
 import app.supernaut.fx.test.NoopBackgroundApp;
+import io.micronaut.context.annotation.Factory;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -27,14 +28,27 @@ import app.supernaut.logging.JavaLoggingSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 import java.io.IOException;
 import java.net.URL;
 
 /**
  * A simple Supernaut.FX App implementing {@link FxForegroundApp}.
+ *
+ * If is also annotated to be Micronaut {@link Factory}. This allows it to
+ * create the {@link Named} {@code String} instance and to specify inclusion
+ * of a {@code Bean} that is defined in a library, which will enable the Micronaut
+ * annotation processor to include the generated code for that {@code Bean}.
+ *
+ * (It might be nice to abstract this "factory" capability somehow so apps don't need to
+ * be  directly dependent on Micronaut for something so simple, but at this point using the @Factory
+ * annotation seems to be the simplest way to do things.)
  */
+
+
 @Singleton
+@Factory
 public class HelloForegroundApp implements FxForegroundApp {
     private static final Logger log = LoggerFactory.getLogger(HelloForegroundApp.class);
     private final FxmlLoaderFactory loaderFactory;
@@ -67,9 +81,19 @@ public class HelloForegroundApp implements FxForegroundApp {
         primaryStage.setTitle("SupernautFX Hello");
         primaryStage.show();
     }
-    
+
+    @Singleton
+    @Named("PERSONNAME")
+    public String getPlanetName() {
+        return "Mars";
+    }
+
+    @Singleton
+    public NoopBackgroundApp getBackgroundApp() {
+        return new NoopBackgroundApp();
+    }
+
     private URL getFXMLUrl(String fileName) {
         return HelloForegroundApp.class.getResource(fileName);
     }
-
 }
