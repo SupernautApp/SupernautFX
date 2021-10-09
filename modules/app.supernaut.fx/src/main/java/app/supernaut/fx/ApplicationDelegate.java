798a4320739c14cf8d858b396a5df3230154d485
Sean Gilligan <sean@msgilligan.com>
2021-10-09 13:42:36 -0700

    Cleanup ApplicationDelegate (missed from last commit)

--- modules/app.supernaut.fx/src/main/java/app/supernaut/fx/ApplicationDelegate.java
+++ modules/app.supernaut.fx/src/main/java/app/supernaut/fx/ApplicationDelegate.java
@@ -15,30 +15,46 @@
  */
 package app.supernaut.fx;
 
-import app.supernaut.ForegroundApp;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
  * Interface for delegated JavaFX applications.
+ * <b>Supernaut.FX</b> applications implement this interface.
+ * <p>
+ * Supernaut.FX applications <i>implement</i> this interface instead of subclassing <b>JavaFX</b> {@link Application}. This has several
+ * advantages over directly extending {@link javafx.application.Application}:
+ * <ol>
+ *     <li>
+ *         Supports flexible construction of application object hierarchies using Dependency Injection provided
+ *         by <b>Micronaut®</b> framework and possibly other D.I. frameworks in the future.
+ *     </li>
+ *     <li>
+ *       Applications {@code implement} an {@code interface} rather than {@code extend} a {@code class}.
+ *       This increases the testability and architectural flexibility of the application.
+ *     </li>
+ *     <li>
+ *         Supports faster, multi-threaded launching with the {@link FxLauncher} interface.
+ *     </li>
+ * </ol>
  */
-public interface ApplicationDelegate extends ForegroundApp {
+public interface ApplicationDelegate {
 
     /**
-     * Pass the JavaFX {@link Application} to the delegate.
+     * Pass the JavaFX {@link Application} to the delegate. Override this method if your application
+     * needs access to {@link javafx.application.HostServices} or other functionality available through
+     * the {@code Application} object.
      * <p>
      * NOTE: This method will be called on the same thread as the JavaFX {@link Application}
      * constructor.
      *
      * @param application A reference to the delegating JavaFX {@link Application} instance
      */
-    default void setApplication(Application application) {
-
-    }
+    default void setApplication(Application application) {}
 
     /**
      * The application initialization method. This method is called from the JavaFX
-     * init() method after the dependency injection context is initialized and the
+     * {@link Application#init()} method after the dependency injection context is initialized and the
      * application is constructed and dependency injected.
      *
      * <p>
@@ -65,6 +81,7 @@ public interface ApplicationDelegate extends ForegroundApp {
     /**
      * This method is called when the application should stop, and provides a
      * convenient place to prepare for application exit and destroy resources.
+     * Called from {@link Application#stop()}
      * <p>
      * NOTE: This method is called on the JavaFX Application Thread.
      *
